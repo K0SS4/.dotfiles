0b57c3ef5b91f3389d37c2acee1787dc275fbb46
K0SS4 <kacper.kossakowski01@gmail.com>
2021-05-07 15:24:37 +0200

    Changed colors in XMobar and some cleanup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,14 @@
-  -- Base
+----------------------------------------
+----------------IMPORTS-----------------
+----------------------------------------
+--Base
 import XMonad
 import System.Directory
 import System.IO (hPutStrLn)
 import System.Exit (exitSuccess)
 import qualified XMonad.StackSet as W
 
-    -- Actions
+-- Actions
 import XMonad.Actions.CopyWindow (kill1)
 import XMonad.Actions.CycleWS (Direction1D(..), moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
 import XMonad.Actions.GridSelect
@@ -14,22 +17,22 @@ import XMonad.Actions.Promote
 import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
 import XMonad.Actions.WithAll (sinkAll, killAll)
 
-    -- Data
+-- Data
 import Data.Monoid
 
-    -- Hooks
+-- Hooks
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
 import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Hooks.SetWMName
 
-    -- Layouts
+-- Layouts
 import XMonad.Layout.SimplestFloat
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.Tabbed
 
-    -- Layouts modifiers
+-- Layouts modifiers
 import XMonad.Layout.LayoutModifier
 import XMonad.Layout.LimitWindows (limitWindows)
 import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
@@ -42,11 +45,15 @@ import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
 import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
 import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
 
-   -- Utilities
+-- Utilities
 import XMonad.Util.Dmenu
 import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
 import XMonad.Util.SpawnOnce
+
+----------------------------------------
+---------------VARIABLES----------------
+----------------------------------------
 
 myFont :: String
 myFont = "xft:SauceCodePro Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
@@ -66,17 +73,6 @@ myNormColor   = "#282c34"   -- Border color of normal windows
 myFocusColor :: String
 myFocusColor  = "#ff0000"   -- Border color of focused windows
 
-myStartupHook :: X ()
-myStartupHook = do
-    spawnOnce "lxsession &"
-    spawnOnce "picom &"
-    spawnOnce "nm-applet &"
-    spawnOnce "volumeicon &"
-    spawnOnce "xfce4-clipman &"
-    spawnOnce "~/.fehbg &"  -- set last saved feh wallpaper
-    spawnOnce "trayer --edge top --align right --widthtype request --padding 5 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 22 &"
-    setWMName "XMonad"
-
 myColorizer :: Window -> Bool -> X (String, String)
 myColorizer = colorRangeFromClassName
                   (0x28,0x2c,0x34) -- lowest inactive bg
@@ -89,45 +85,40 @@ myColorizer = colorRangeFromClassName
 mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
 mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
 
--- Defining a bunch of layouts, many that I don't use.
--- limitWindows n sets maximum number of windows displayed for layout.
--- mySpacing n sets the gap size around the windows.
-tall     = renamed [Replace "tall"]
+----------------------------------------
+---------------AUTOSTART----------------
+----------------------------------------
+
+myStartupHook :: X ()
+myStartupHook = do
+    spawnOnce "lxsession &"
+    spawnOnce "picom &"
+    spawnOnce "nm-applet &"
+    spawnOnce "volumeicon &"
+    spawnOnce "xfce4-clipman &"
+    spawnOnce "~/.fehbg &"  -- set last saved feh wallpaper
+    spawnOnce "trayer --edge top --align right --widthtype request --padding 5 --monitor 1 --transparent true --alpha 120 --tint 0x282c34 --height 24 &"
+    setWMName "XMonad"
+
+----------------------------------------
+----------------LAYOUTS-----------------
+----------------------------------------
+
+tall     =   renamed [Replace "tall"]
            $ smartBorders
            $ addTabs shrinkText myTabTheme
            $ limitWindows 12
            $ mySpacing 8
            $ ResizableTall 1 (3/100) (1/2) []
-monocle  = renamed [Replace "monocle"]
+monocle  =   renamed [Replace "monocle"]
            $ smartBorders
            $ addTabs shrinkText myTabTheme
            $ limitWindows 20 Full
-floats   = renamed [Replace "floats"]
+floats   =   renamed [Replace "floats"]
            $ smartBorders
            $ limitWindows 20 simplestFloat
-tabs     = renamed [Replace "tabs"]
-           -- I cannot add spacing to this layout because it will
-           -- add spacing between window and tabs which looks bad.
+tabs     =   renamed [Replace "tabs"]
            $ tabbed shrinkText myTabTheme
-
--- setting colors for tabs layout and tabs sublayout.
-myTabTheme = def { fontName            = myFont
-                 , activeColor         = "#46d9ff"
-                 , inactiveColor       = "#313846"
-                 , activeBorderColor   = "#46d9ff"
-                 , inactiveBorderColor = "#282c34"
-                 , activeTextColor     = "#282c34"
-                 , inactiveTextColor   = "#d0d0d0"
-                 }
-
--- Theme for showWName which prints current workspace when you change workspaces.
-myShowWNameTheme :: SWNConfig
-myShowWNameTheme = def
-    { swn_font              = "xft:Font Awesome 5 Free Solid:size=100"
-    , swn_fade              = 0.8
-    , swn_bgcolor           = "#1c1f24"
-    , swn_color             = "#ffffff"
-    }
 
 -- The layout hook
 myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats
@@ -138,8 +129,36 @@ myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts float
                                  ||| floats
                                  ||| noBorders tabs
 
+-- setting colors for tabs layout.
+myTabTheme = def { fontName            = myFont
+                 , activeColor         = "#46d9ff"
+                 , inactiveColor       = "#313846"
+                 , activeBorderColor   = "#46d9ff"
+                 , inactiveBorderColor = "#282c34"
+                 , activeTextColor     = "#282c34"
+                 , inactiveTextColor   = "#d0d0d0"
+                 }
+
+----------------------------------------
+---------------WORKSPACES---------------
+----------------------------------------
+
+-- Theme for showWName which prints current workspace when you change workspaces.
+myShowWNameTheme :: SWNConfig
+myShowWNameTheme = def
+    { swn_font              = "xft:Font Awesome 5 Free Solid:size=100"
+    , swn_fade              = 0.8
+    , swn_bgcolor           = "#1c1f24"
+    , swn_color             = "#ffffff"
+    }
+
+
 -- myWorkspaces = [" 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 "]
 myWorkspaces = ["  \xf7a2  ", "  \xf120  ", "  \xf013  ", "  \xf1c2  ", "  \xf11b  ", "  \xf0eb  "]
+
+----------------------------------------
+--------------MANAGE HOOK---------------
+----------------------------------------
 
 myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
 myManageHook = composeAll
@@ -163,6 +182,10 @@ myManageHook = composeAll
      , (className =? "firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
      , isFullscreen -->  doFullFloat
      ] 
+
+----------------------------------------
+-----------------KEYS-------------------
+----------------------------------------
 
 myKeys :: [(String, X ())]
 myKeys =
@@ -215,7 +238,7 @@ myKeys =
         , ("M-M1-j", sendMessage MirrorShrink)          -- Shrink vert window width
         , ("M-M1-k", sendMessage MirrorExpand)          -- Expand vert window width
 
-    -- Set wallpaper with 'feh'. Type 'SUPER+F1' to launch sxiv in the wallpapers directory.
+    -- Type 'SUPER+F1' to launch sxiv in the wallpapers directory.
         , ("M-<F1>", spawn "sxiv -r -q -t -o ~/.images/*")
 
     -- Multimedia Keys
@@ -224,6 +247,10 @@ myKeys =
         , ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse set Master 5%+ unmute")
         , ("<Print>", spawn "xfce4-screenshooter")
         ]
+
+----------------------------------------
+-----------------MAIN-------------------
+----------------------------------------
 
 main :: IO ()
 main = do
@@ -245,13 +272,13 @@ main = do
               -- the following variables beginning with 'pp' are settings for xmobar.
               { ppOutput = \x -> hPutStrLn xmproc0 x                          -- xmobar on monitor 1
                               >> hPutStrLn xmproc1 x                          -- xmobar on monitor 2
-              , ppCurrent = xmobarColor "#98be65" "" . wrap "[" "]"           -- Current workspace
-              , ppVisible = xmobarColor "#98be65" ""                          -- Visible but not current workspace
-              , ppHidden = xmobarColor "#82AAFF" "" . wrap "*" ""             -- Hidden workspaces
-              , ppHiddenNoWindows = xmobarColor "#c792ea" ""                  -- Hidden workspaces (no windows)
+              , ppCurrent = xmobarColor "#96b869" "" . wrap "[" "]"           -- Current workspace
+              , ppVisible = xmobarColor "#96b869" ""                          -- Visible but not current workspace
+              , ppHidden = xmobarColor "#dfe683" "" . wrap "*" ""             -- Hidden workspaces
+              , ppHiddenNoWindows = xmobarColor "#e6a80b" ""                  -- Hidden workspaces (no windows)
               , ppTitle = const ""                                            -- Title of active window
               , ppTitleSanitize = const ""
-              , ppSep =  "<fc=#666666><fn=1>|</fn> </fc>"                    -- Separator character
-              , ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!"            -- Urgent workspace
+              , ppSep =  "<fc=#ffffff><fn=1>|</fn> </fc>"                    -- Separator character
+              , ppUrgent = xmobarColor "#ff0000" "" . wrap "!" "!"            -- Urgent workspace
               }
         } `additionalKeysP` myKeys
