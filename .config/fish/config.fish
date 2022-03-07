efb5dd45ca031fdee39aa35e84fd895422c1d7d4
K0SS4 <kacper.kossakowski01@gmail.com>
2022-03-08 00:00:56 +0100

    Small changes in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -41,13 +41,13 @@ end
 alias doas="doas --"
 
 # windscribe
-alias conn='sudo systemctl start windscribe;windscribe connect'
+alias conn='sudo systemctl start windscribe;sleep 1;windscribe connect'
 alias dis='windscribe disconnect; sudo systemctl stop windscribe'
 alias winstat='windscribe status'
 
 #firefox profile in ram
-alias poweroff='/home/k0ss4/.local/bin/firefox-sync.sh pz8i377i.default-release >> /dev/null; poweroff'
-alias reboot='/home/k0ss4/.local/bin/firefox-sync.sh pz8i377i.default-release >> /dev/null; reboot'
+#alias poweroff='/home/k0ss4/.local/bin/firefox-sync.sh pz8i377i.default-release >> /dev/null; poweroff'
+#alias reboot="/home/k0ss4/.local/bin/firefox-sync.sh pz8i377i.default-release >> /dev/null; reboot"
 
 #poweroff disk
 alias remove="udisksctl power-off -b"
