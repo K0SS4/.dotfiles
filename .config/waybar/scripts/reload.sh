9ceb19f25a0b168988d1db48eb76cee40e2b7125
K0SS4 <github@k0ss4.xyz>
2023-11-03 19:43:50 +0100

    Separated waybar configs for separate monitors(one without waybar-updates). Separated exec-once in hyprland config. Added missing reload waybar script.

---
+++ .config/waybar/scripts/reload.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+killall waybar;
+pid=$(pgrep waybar)
+
+while [ "$pid" != "" ];
+do
+    pid=$(pgrep waybar)
+done
+
+if [ $(pgrep "Hyprland") != "" ]; 
+then
+    waybar -c ~/.config/waybar/config-hypr-1 -s ~/.config/waybar/style-hypr.css &
+else
+    waybar -c ~/.config/waybar/config-sway -s ~/.config/waybar/style-sway.css &
+fi
+
+swaync-client -R;
